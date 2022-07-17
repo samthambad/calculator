ca39ec53d064606d9c64578a70b49ebc5d231c95
sam <thambad@gmail.com>
2022-07-17 16:30:34 +0800

    Reduced the number of buttonClick functions by adding a variable

--- calculator.js
+++ calculator.js
@@ -31,64 +31,13 @@ function operate(a,operator,b){//remember to keep operator in string
     }
 }
 
+
+
 //functions run when number buttons are clicked.
-function buttonClick1(){
+
+function buttonClick(num){
     const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 1
-    display1.appendChild(oneEntered);
-}
-function buttonClick2(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 2
-    display1.appendChild(oneEntered);
-}
-function buttonClick3(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 3
-    display1.appendChild(oneEntered);
-}
-function buttonClick4(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 4
-    display1.appendChild(oneEntered);
-}
-function buttonClick5(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 5
-    display1.appendChild(oneEntered);
-}
-function buttonClick6(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 6
-    display1.appendChild(oneEntered);
-}
-function buttonClick7(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 7
-    display1.appendChild(oneEntered);
-}
-function buttonClick8(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 8
-    display1.appendChild(oneEntered);
-}
-function buttonClick9(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 9
-    display1.appendChild(oneEntered);
-}
-function buttonClick0(){
-    const display1 = document.querySelector('.display')
-    const oneEntered = document.createElement('span')
-    oneEntered.textContent = 0
-    display1.appendChild(oneEntered);
+    const numEntered = document.createElement('span')
+    numEntered.textContent = num
+    display1.appendChild(numEntered);
 }
